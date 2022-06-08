f17082dfd03713d78737abeaf7a899f505c8120f
hieunghia-pat <19520178@gm.uit.edu.vn>
2022-06-08 08:18:14 +0700

    redefined permissions for apis

--- web/src/main/java/uit/spring/annotation/security/UserPermission.java
+++ web/src/main/java/uit/spring/annotation/security/UserPermission.java
@@ -10,7 +10,9 @@ public enum UserPermission {
     ADMIN_READ("admin:read"),
     ADMIN_WRITE("admin:write"),
     SUBSET_READ("subset:read"),
-    SUBSET_WRITE("subset:write");
+    SUBSET_WRITE("subset:write"),
+    ASSIGNMENT_READ("assignment:read"),
+    ASSIGNMENT_WRITE("assignment:write");
 
     private final String permission;
 
