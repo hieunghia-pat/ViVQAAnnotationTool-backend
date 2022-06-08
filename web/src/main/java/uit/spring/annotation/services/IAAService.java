d0112c5986913ae5cb8e197bdf6b29e18694ba9f
TranNam <trandinhnam071021@gmail.com>
2022-06-08 11:48:50 +0700

    Added IAA Calculation

--- web/src/main/java/uit/spring/annotation/services/IAAService.java
+++ web/src/main/java/uit/spring/annotation/services/IAAService.java
@@ -11,6 +11,7 @@ import uit.spring.annotation.repositories.AnnotationRepository;
 import uit.spring.annotation.repositories.ImageRepository;
 import uit.spring.annotation.repositories.UserSubsetRepository;
 
+import javax.swing.text.html.Option;
 import java.util.*;
 
 @Slf4j
@@ -38,11 +39,14 @@ public class IAAService {
         List<Long> imageIdList = new ArrayList<>();
         Set<UUID> userIdSet = new HashSet<>();
         Map<Long, Object> imageAnnotation = new HashMap<>();
-        Map<UUID, Object> userAnnotation = new HashMap<>();
 
+        int nQA = 0;
+
+        //Get image ID list
         for(Image image:imageList){
             imageIdList.add(image.getId());
         }
+        //Get user ID set
         for(UserSubset userSubset:userSubsetsList){
             userIdSet.add(userSubset.getUser().getId());
         }
@@ -50,12 +54,34 @@ public class IAAService {
         for(Long imageId:imageIdList){
             for(UUID userId:userIdSet){
                 Optional<Annotation> annotationOptional = annotationRepository.findByUserForImage(userId, imageId);
-//                Map<UUID, Object> userAnnotation = new HashMap<>();
+                Map<UUID, Object> userAnnotation = new HashMap<>();
                 userAnnotation.put(userId, annotationOptional);
                 imageAnnotation.put(imageId, userAnnotation);
             }
         }
 
-        return userAnnotation;
+        //Count number of annotation
+        for(Image image:imageList){
+            if(!image.isToDelete()){
+                nQA++;
+            }
+        }
+
+        ArrayList<ArrayList<Integer>> answerType = new ArrayList<>(3);
+        for(int i = 0; i < nQA; i++){
+            answerType.add(new ArrayList<>());
+        }
+
+        //Create table Answer Type
+        for(Long imageId:imageIdList){
+            Optional<Image> image = imageRepository.findById(imageId);
+            if(!image.get().isToDelete()){
+                for(UUID userId:userIdSet) {
+                    Object annotation = imageAnnotation.get(imageId);
+                }
+            }
+        }
+
+        return imageAnnotation;
     }
 }
